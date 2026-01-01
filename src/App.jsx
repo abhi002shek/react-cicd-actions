65970164737d3bb841040f0f61c9d22ff05cf09f
Abhishek <abhishek@Abhisheks-MacBook-Pro.local>
2026-01-01 14:03:37 +0530

    Added new feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ function App() {
 
   return (
     <>
-      <h1>Github Actions</h1>
+      <h1>Github Actions and Sonarcloud</h1>
       <h2>Happy New year 2026, You have a great journey ahead!</h2>
-      <h5>with Sonarcloud</h5>
+      <h5>Keep smiling</h5>
     </>
   )
 }
